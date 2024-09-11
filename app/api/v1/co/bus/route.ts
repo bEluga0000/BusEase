e8e4c0105ed6d0408c2a082fd9c06e515ec3cf0e
shreyas20003 <shreyasas2975@gmail.com>
2024-09-11 11:46:43 +0530

    change

--- app/api/v1/co/bus/route.ts
+++ app/api/v1/co/bus/route.ts
@@ -47,58 +47,58 @@ export async function POST(req: NextRequest) {
     }
 }
 
-export async function GET(req: NextRequest) {
-    try {
-        const serverSession = await getServerSession({req,...NEXT_AUTH})
-        console.log(serverSession)
-        if (!serverSession)
-            return NextResponse.json({ msg: "Please Login before hitting this request", err: "Access denied" }, { status: 403 })
-        if (serverSession.user.role == "user")
-            return NextResponse.json({ msg: "your not a company owner", err: "Access denied" }, { status: 403 })
-        const companyId  = req.headers.get("companyId")
-        const busId = req.headers.get("busId")
-        const parsedData = getBusSchema.safeParse({companyId,busId})
-        if (!parsedData.success)
-            return NextResponse.json({ msg: "Enter valid Data", err: parsedData.error.errors }, { status: 400 })
-        else
-        {
-            if(!parsedData.data.busId)
-            {
-                const buses = await prisma.bus.findMany({
-                    where: {
-                        companyId: parsedData.data.companyId,
-                        comapny:{
-                            userId:serverSession.user.id
-                        }
-                    }
-                })
-                if (!buses)
-                    return NextResponse.json({ msg: "Buses not found" }, { status: 404 })
-                else
-                    return NextResponse.json({ msg: "All buses of company", buses })
-            }
-            else
-            {
-                const buses = await prisma.bus.findUnique({
-                    where: {
-                        companyId: parsedData.data.companyId,
-                        busId:parsedData.data.busId,
-                        comapny: {
-                            userId: serverSession.user.id
-                        }
-                    },include:{
-                        seats:true
-                    }
-                })
-                if (!buses)
-                    return NextResponse.json({ msg: "Buse not found" }, { status: 404 })
-                else
-                    return NextResponse.json({ msg: "successfully got bus", buses })
-            }
+// export async function GET(req: NextRequest) {
+//     try {
+//         const serverSession = await getServerSession({req,...NEXT_AUTH})
+//         console.log(serverSession)
+//         if (!serverSession)
+//             return NextResponse.json({ msg: "Please Login before hitting this request", err: "Access denied" }, { status: 403 })
+//         if (serverSession.user.role == "user")
+//             return NextResponse.json({ msg: "your not a company owner", err: "Access denied" }, { status: 403 })
+//         const companyId  = req.headers.get("companyId")
+//         const busId = req.headers.get("busId")
+//         const parsedData = getBusSchema.safeParse({companyId,busId})
+//         if (!parsedData.success)
+//             return NextResponse.json({ msg: "Enter valid Data", err: parsedData.error.errors }, { status: 400 })
+//         else
+//         {
+//             if(!parsedData.data.busId)
+//             {
+//                 const buses = await prisma.bus.findMany({
+//                     where: {
+//                         companyId: parsedData.data.companyId,
+//                         comapny:{
+//                             userId:serverSession.user.id
+//                         }
+//                     }
+//                 })
+//                 if (!buses)
+//                     return NextResponse.json({ msg: "Buses not found" }, { status: 404 })
+//                 else
+//                     return NextResponse.json({ msg: "All buses of company", buses })
+//             }
+//             else
+//             {
+//                 const buses = await prisma.bus.findUnique({
+//                     where: {
+//                         companyId: parsedData.data.companyId,
+//                         busId:parsedData.data.busId,
+//                         comapny: {
+//                             userId: serverSession.user.id
+//                         }
+//                     },include:{
+//                         seats:true
+//                     }
+//                 })
+//                 if (!buses)
+//                     return NextResponse.json({ msg: "Buse not found" }, { status: 404 })
+//                 else
+//                     return NextResponse.json({ msg: "successfully got bus", buses })
+//             }
             
-        }
+//         }
 
-    } catch (e: any) {
-        return NextResponse.json({ msg: "Something went wrong", err: e.message }, { status: 500 })
-    }
-}
+//     } catch (e: any) {
+//         return NextResponse.json({ msg: "Something went wrong", err: e.message }, { status: 500 })
+//     }
+// }
